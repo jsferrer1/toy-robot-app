18cc823eef2b995e6a3ae9dba7322fdf56b9e7f7
jsferrer1 <jsferrer1@yahoo.com>
2021-02-06 22:14:48 +0800

    paper grid

--- src/GridBoard.js
+++ src/GridBoard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexWrap: 'wrap',
+    justifyContent: "center",    
     '& > *': {
       margin: theme.spacing(0.2),
       width: theme.spacing(10),
@@ -17,25 +17,35 @@ const useStyles = makeStyles((theme) => ({
 
 const arrayFromInteger = (range) => Array.from(Array(range).keys(), i => i);
 
+const createEmpty2DArray = (height, width) => {
+  let data = [];
+  for (let i = 0; i < height; i++) {
+    data.push([]);
+    for (let j = 0; j < width; j++) {
+      data[i][j] = {
+        x: i,
+        y: j,
+        isBotHere: false,
+      };
+    }
+  }
+  return data;
+}
+
 export default function GridBoard() {
   const classes = useStyles();
 
   const rows = arrayFromInteger(5);
   const columns = arrayFromInteger(5);
 
-  const board = {
-    rows: rows,
-    columns: columns
-  }
-
   return (
     <div className={classes.root}>
-      {rows.map(() => (
-        <div className={classes.root}>
-          {columns.map(() => (
-            <Paper elevation={0} />
-          ))}
-        </div>
+      {rows.map((row) => (
+        <Grid>
+          {columns.map((col) => (
+            <Paper id={"row_"+col+"_col_"+row} variant="outlined" align="center" square />
+            ))}
+        </Grid>
       ))}
     </div>
   );

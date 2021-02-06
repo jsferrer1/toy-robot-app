00c9cbb45db7fce88cb370ed886d481e8b355348
jsferrer1 <jsferrer1@yahoo.com>
2021-02-07 01:34:45 +0800

    mapFace

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/styles";
-import Button from '@material-ui/core/Button';
 
 import {
   AppBar,
@@ -17,18 +16,6 @@ const theme = createMuiTheme({
   }
 });
 
-const placeRobot = () => {
-  document.getElementById('row_0_col_0').innerHTML = '<p>bot</p>';
-}
-
-const moveRobot = () => {
-  document.getElementById('row_0_col_0').innerHTML = '<p>bot</p>';
-}
-
-const hideRobot = () => {
-  document.getElementById('row_0_col_0').innerHTML = '';
-}
-
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
@@ -40,11 +27,6 @@ const App = () => {
         &nbsp;
       </Typography>
       <GridBoard />
-      <Typography style={{ marginTop: 350 }}>
-        <Button onClick={() => {placeRobot()}}>place</Button>
-        <Button onClick={() => {moveRobot()}}>move</Button>
-        <Button onClick={() => {hideRobot()}}>hide</Button>
-      </Typography>
     </ThemeProvider>
   );
 };
--- src/GridBoard.js
+++ src/GridBoard.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { Typography } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import MaskedInput from 'react-text-mask';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: "center",    
+    // flexWrap: 'wrap',
     '& > *': {
       margin: theme.spacing(0.2),
-      width: theme.spacing(10),
-      height: theme.spacing(10),
+      width: theme.spacing(20),
+      height: theme.spacing(20),
     },
   },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: 70,
+    width: 70,
+    textAlign: 'center',
+  },  
 }));
+
+const PlaceInput = ({ inputRef, ...props }) => (
+  <MaskedInput
+    {...props}
+    ref={ref => {
+      inputRef(ref ? ref.inputElement : null);
+    }}
+    mask={[
+      /[1-5]/,
+      ',',
+      /[1-5]/,
+      ',',
+      /[A-Z]/,
+      /[A-Z]/,
+      /[A-Z]/,
+      /[A-Z]/,
+      /[A-Z]/,
+    ]}
+    placeholderChar={'\u2000'}
+  />
+);
 
 const arrayFromInteger = (range) => Array.from(Array(range).keys(), i => i);
 
@@ -25,6 +58,7 @@ const createEmpty2DArray = (height, width) => {
       data[i][j] = {
         x: i,
         y: j,
+        facingDirection: '',
         isBotHere: false,
       };
     }
@@ -32,21 +66,95 @@ const createEmpty2DArray = (height, width) => {
   return data;
 }
 
+
+
+/**
+ * main function
+ */
+
 export default function GridBoard() {
   const classes = useStyles();
+  const [placeCommand, setPlaceCommand] = useState('');
+  const [currentGridId, setCurrentGridId] = useState('');
 
   const rows = arrayFromInteger(5);
   const columns = arrayFromInteger(5);
 
+  const mapFace = {
+    'NORTH' : '^',
+    'EAST' : '>',
+    'WEST' : '<',
+    'SOUTH' : 'V'
+  }
+
+  const handleCommand = (e) => {
+    if (e.key === 'Enter') {
+      setPlaceCommand(e.target.value);
+      e.preventDefault();
+    }
+  }
+
+  const placeRobot = () => {
+    console.log('placeCommand: ', placeCommand);
+    let [x,y,f] = placeCommand.trim().split(',');
+    console.log('x: ', x--);
+    console.log('y: ', y--);
+    console.log('f: ', f);
+    console.log('facing: ', mapFace[f]);
+    const gridId = 'x_' + x + '_y_' + y;
+    document.getElementById(gridId).innerHTML = '<p>'+mapFace[f]+'</p>';
+    setCurrentGridId(gridId);
+    // disable command and place
+  }
+
+  const moveRobot = () => {
+    document.getElementById('x_0_y_0').innerHTML = '<p>bot</p>';
+  }
+  
+  const hideRobot = () => {
+    document.getElementById('x_0_y_0').innerHTML = '';
+  }
+      
+  const reset = () => {
+    window.location.reload();
+  }
+      
   return (
-    <div className={classes.root}>
-      {rows.map((row) => (
-        <Grid>
-          {columns.map((col) => (
-            <Paper id={"row_"+col+"_col_"+row} variant="outlined" align="center" square />
-            ))}
+    <Grid container className={classes.root} spacing={1}>
+      <Grid item xs={12}>
+        {rows.map((x) => (
+        <Grid container justify="center" spacing={1}>
+          {columns.map((y) => (
+            <Grid key={y} item>
+              <Paper 
+               className={classes.paper} 
+               id={"x_"+x+"_y_"+y}
+              />
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </div>
+        ))}
+      </Grid>
+      <Typography style={{ marginTop: 200 }}>
+        &nbsp;
+      </Typography>
+      <Grid container align="left" spacing={1}>
+        <Grid item xs={12} align="left">
+          <TextField 
+           id="placeCommand" 
+           label="Command" 
+           onKeyPress={(e) => {handleCommand(e)}}
+           InputProps={{ inputComponent: PlaceInput }}
+          />          
+          <br/><br/>
+          <Button onClick={() => {placeRobot()}}>place</Button>
+          <Button onClick={() => {moveRobot()}}>move</Button>
+          <Button onClick={() => {hideRobot()}}>left</Button>
+          <Button onClick={() => {hideRobot()}}>right</Button>
+          <Button onClick={() => {hideRobot()}}>report</Button>
+          <Button onClick={() => {reset()}}>reset</Button>
+        </Grid>
+      </Grid>
+    </Grid>      
   );
 }
